4ce12f2de2acac1f5362c1f4b084591517b5d936
AxelMorales2010 <yamilaxel@gmail.com>
2022-03-27 16:19:25 -0600

    Package Refactor

---
+++ src/main/java/dev/c20/ecommerce/ApplicationConfiguration.java
@@ -0,0 +1,21 @@
+package dev.c20.ecommerce;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+
+@SpringBootApplication
+
+public class ApplicationConfiguration {
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationConfiguration.class, args);
+    }
+
+}
